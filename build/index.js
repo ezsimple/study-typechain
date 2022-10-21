f027f757c9d51e1499d885b2d0fffb32e85f0a99
mhlee <mkeasy@gmail.com>
2022-10-21 17:05:35 +0900

    init

---
+++ build/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const msg = 'hello';
+console.log(msg);
---
+++ src/myPackage.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {string} config
+ * @param {boolean} config
+ * @returns {boolean}
+ */
+export function init(config) {
+  return true;
+}
+
+/**
+ * Exit block
+ * @param {number} code
+ * @returns {number}
+ */
+export function exit(code) {
+  return code;
+}
